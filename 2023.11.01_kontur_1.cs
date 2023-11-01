a410ba6e6816a8ee2d5ad88f4af5edb4205ee5fd
Motya98 <113919191+Motya98@users.noreply.github.com>
2023-11-01 22:55:57 +0500

    Update 2023.11.01_kontur_1.cs

--- 2023.11.01_kontur_1.cs
+++ 2023.11.01_kontur_1.cs
@@ -2,14 +2,14 @@ class Program
 {
     static void Main()
     {
-        var inp = Console.ReadLine().Split(" ");
-        double multiply = double.Parse(inp[0]) * uint.Parse(inp[1]);
+        var inp = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+        var multiply = inp[0] * inp[1];
         var result = 0;
         while (true)
         {
             if (multiply % 2 == 0)
             {
-                multiply = multiply / 2;
+                multiply /= 2;
                 result++;
             }
             else

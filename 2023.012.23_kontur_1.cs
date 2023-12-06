5eb3098edbb85fd089aedc538acdc1829e405bfc
Motya98 <113919191+Motya98@users.noreply.github.com>
2023-12-07 00:04:11 +0500

    Create 2023.012.23_kontur_1.cs

---
+++ 2023.012.23_kontur_1.cs
@@ -0,0 +1,15 @@
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var inp = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        var n = inp[0];
+        var m = inp[1];
+        if (n == m)
+            Console.WriteLine(n + m * 2);
+        else if (n > m)
+            Console.WriteLine(m + 1 + m * 2);
+        else
+            Console.WriteLine(n + (n + 1) * 2);
+    }
+}

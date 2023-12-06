4b2071ec263c811c433bb8e9e9f610fa1be160f2
Motya98 <113919191+Motya98@users.noreply.github.com>
2023-12-07 00:06:01 +0500

    Create 2023.012.23_kontur_2.cs

---
+++ 2023.012.23_kontur_2.cs
@@ -0,0 +1,40 @@
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var arrayLength = int.Parse(Console.ReadLine());
+        var array = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+        var startIndex = 0;
+        var endIndex = arrayLength - 1;
+        var startSum = array[startIndex];
+        var endSum = array[endIndex];
+        
+        while(startIndex + 1 != endIndex)
+        {
+            if (startSum >= endSum)
+                endSum += array[--endIndex];
+            else if (startSum < endSum)
+                startSum += array[++startIndex];
+        }
+
+        if (startSum == endSum)
+        {
+            for (var i = 0; i < arrayLength; i++)
+            {
+                if (i < startIndex || (i > startIndex && i != arrayLength - 1))
+                    Console.Write($"{array[i]}+");
+                else if(i == startIndex)
+                    Console.Write($"{array[i]}=");
+                else
+                    Console.Write(array[i]);
+            }
+        }
+        else
+            Console.WriteLine(-1);
+    }
+}
+
+/*
+5
+8 1 1 5 5
+*/
